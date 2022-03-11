4a2160b556ec65f86129c75560002b76a14218ce
Jason Hamilton <jason.hamilton@thefuzz4.net>
2022-03-11 00:05:08 +0000

    Fixed

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "app.terraform.io/instruqt-circleci/network/azurerm"
+  source  = "source  = app.terraform.io/thefuzz4_terraform/network/azurerm"
   version = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 
